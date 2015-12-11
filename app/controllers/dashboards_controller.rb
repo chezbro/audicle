63e7ea39d0ddc40370edd99543de39724b6020e3
EMC <ericchesbrough@gmail.com>
2015-12-10 22:30:34 -0600

    database

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,8 +3,8 @@ class DashboardsController < ApplicationController
   def index
     if user_signed_in?
       # @recordings  = current_user.recordings.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
       @user = current_user
+      @feed_items = current_user.feed.paginate(page: params[:page])
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   acts_as_followable
   acts_as_follower
   acts_as_voter
+  
   # has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   # has_many :followed_users, through: :relationships, source: :followed
   # has_many :reverse_relationships, foreign_key: "followed_id",
